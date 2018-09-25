7904e1b0256575bf9eab2c92c01ce6d52b780e5b
Maddie Jennings <madisonlynnjennings@gmail.com>
2018-09-25 15:07:59 -0500

    added mapbox and webpacks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-const index = require('../public/bundle')
+// const index = require('../public/bundle')
+console.log('Hello from JavaScript');
 
+const mapboxgl = require("mapbox-gl");
 
-console.log("Hello from JavaScript");
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFkZGllZWlkZGFtIiwiYSI6ImNqbWk1ZG9tejAxZW0zb3JwMmk5NXJ6cnoifQ.ZDIkuER2XZwMilQIqlRp_A';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
